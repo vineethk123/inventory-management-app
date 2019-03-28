4aa54890dcfe7af7f6271f895a939c2023c91631
vineethk123 <vineeth.kolluru@zemosolabs.com>
2019-03-28 16:24:35 +0530

    add server module

---
+++ inventory-management-app-server-module/src/main/java/com/usecase/inventorymanagementapp/InventoryManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.usecase.inventorymanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementAppApplication.class, args);
+	}
+
+}
