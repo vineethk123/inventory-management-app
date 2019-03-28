621e9ae66231176226d3c953a0a53d2ecbd97058
vineethk123 <vineeth.kolluru@zemosolabs.com>
2019-03-28 17:28:36 +0530

    Write API to get only available inventories

--- inventory-management-app-server-module/src/main/java/com/usecase/inventorymanagementapp/controller/InventoryController.java
+++ inventory-management-app-server-module/src/main/java/com/usecase/inventorymanagementapp/controller/InventoryController.java
@@ -1,4 +1,21 @@
 package com.usecase.inventorymanagementapp.controller;
 
+import com.usecase.inventorymanagementapp.model.Inventory;
+import com.usecase.inventorymanagementapp.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/fun-inventory")
 public class InventoryController {
+
+    @Autowired
+    InventoryService inventoryService;
+
+    @GetMapping("/available-inventory")
+    public List<Inventory> getAvailableInventory() {
+        return inventoryService.getAvailableInventory();
+    }
 }
--- inventory-management-app-server-module/src/main/java/com/usecase/inventorymanagementapp/model/Inventory.java
+++ inventory-management-app-server-module/src/main/java/com/usecase/inventorymanagementapp/model/Inventory.java
@@ -15,9 +15,37 @@ public class Inventory {
     @Column(name = "name", nullable = false)
     String inventoryName;
 
-    public Inventory(Long id, String inventoryName) {
+    @Column(name = "status", nullable = false)
+    String availabilityStatus;
+
+    @Column(name = "employeeName", nullable = false)
+    String employeName;
+
+    public Inventory(Long id, String inventoryName, String availabilityStatus, String employeName) {
         this.id = id;
         this.inventoryName = inventoryName;
+        this.availabilityStatus = availabilityStatus;
+        this.employeName = employeName;
+    }
+
+    public void setInventoryName(String inventoryName) {
+        this.inventoryName = inventoryName;
+    }
+
+    public String getAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public void setAvailabilityStatus(String availabilityStatus) {
+        this.availabilityStatus = availabilityStatus;
+    }
+
+    public String getEmployeName() {
+        return employeName;
+    }
+
+    public void setEmployeName(String employeName) {
+        this.employeName = employeName;
     }
 
     public Long getId() {
